2341da6db34ce6fb2cbb4fca439ae4d231d054eb
francardoso <fran.adalid@hotmail.com>
2019-11-25 09:38:17 -0300

    process env port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen('3028', console.log('foo server lisening on 3028'));
+app.listen(process.env.PORT || 3028, console.log(`foo server lisening on ${process.env.PORT || 3028}`));
 
 app.get('/', (req, res) =>{
     res.send('<h1> Olá mundo! </h1>');
